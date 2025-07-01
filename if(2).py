9a6b3ab8081031b12ec0d54c45edb7bab664a7ca
Mohd Hammad Ali <gtaali206@gmail.com>
2025-07-01 21:02:40 +0530

    if(2).py

--- if(2).py
+++ if(2).py
@@ -1,3 +1,11 @@
+# 🍬 Sugar Level Checker
+
+#A simple Python program to check if your sugar level is low, high, or normal based on user input.
+
+## 💡 Source
+
+#Inspired by [https://github.com/codebasics/py/tree/master/Basics/Exercise].
+
 sugar = float(input("Type on your sugar level: -")) 
 if sugar < 80:
     print("Your sugar level is low by",80-sugar)
